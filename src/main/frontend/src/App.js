c17a5d2794850461af05df6184eede7fcfd54bf3
breath03 <baekjimin0301@gmail.com>
2024-09-08 14:36:26 +0900

    main화면구현 진행중 백지민

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,7 +1,31 @@
 // import logo from './logo.svg';
 import React, {useEffect, useState} from "react";
 import axios from 'axios';
-// import './App.css';
+import './App.css';
+
+function Header() {
+  return (
+    <header>
+      <div id="header_bar">
+        <div id="menu_bar">
+          <div id="logo_icon"></div>
+          <div id="home_link">홈</div>
+          <div id="information_link">정보</div>
+          <div id="community_link">게시판</div>
+          <div id="review_link">리뷰</div>
+        </div>
+        <div id="alarm_button_section">
+          <div id="alarm_button"></div>
+        </div>
+        <div id="message_button_section">
+          <div id="message_button"></div>
+        </div>
+        <div id="login_button">8</div>
+        <div id="logout_button">9</div>
+      </div>
+    </header>
+  )
+}
 
 function App() {
   const [hello, setHello] = useState('')
@@ -15,9 +39,14 @@ function App() {
   }, []);
 
   return (
+    <>
+    <main>
+      <Header></Header>
       <div>
         이곳은 테스트 입니다.ㅇㅇㅇ : {hello}
       </div>
+    </main>
+    </>
   );
 }
 
