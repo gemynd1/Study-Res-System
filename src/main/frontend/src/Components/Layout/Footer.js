48fcc353726f95f56ecc9eb5c892f6aaa5158e8a
breath03 <baekjimin0301@gmail.com>
2024-09-10 20:44:10 +0900

    footer디자인 진행중 백지민

--- src/main/frontend/src/Components/Layout/Footer.js
+++ src/main/frontend/src/Components/Layout/Footer.js
@@ -7,20 +7,20 @@ const Footer = () => {
         <>
             <footer className="footer">
                 <ul className="footer-section-1">
-                    <li><img src="/img/snlogo.png" alt="logo" className="logo"/></li>
-                    <li>
-                        <div>SN스터디</div>
-                        <div>경기도 안양시 만안구 양화로37번길 34</div>
+                    <li><img src="/img/snlogo(white).png" alt="logo" className="logo"/></li>
+                    <li className="footer-address">
+                        <div className="footer-address-1">SN스터디</div>
+                        <div className="footer-address-2">경기도 안양시 만안구 양화로37번길 34</div>
                     </li>
-                    <li>
-                        <div>팀원</div>
-                        <div>김태랑 백지민 정희수 김지민</div>
+                    <li className="footer-team">
+                        <div className="footer-team-1">팀원</div>
+                        <div className="footer-team-2">김태랑 백지민 정희수 김지민</div>
                     </li>
-                    <li>
-                        <div>Email</div>
-                        <div>muse7244@yeonsung.ac.kr</div>
+                    <li className="footer-email">
+                        <div className="footer-email-1">Email</div>
+                        <div className="footer-email-2">muse7244@yeonsung.ac.kr</div>
                     </li>
-                    <li>Copyright 2024 SN STUDY inc. Allright Reserved.</li>
+                    <li className="footer-copyright">Copyright 2024 SN STUDY inc. Allright Reserved.</li>
                 </ul>
                 <ul className="footer-section-2">
                     <li>회사명: SN</li>
