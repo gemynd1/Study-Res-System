c1de0f97c68cf14c86fe3ac3160651c16eaf69b7
taerangkim <taerangkim0116@gmail.com>
2024-09-10 10:21:11 +0900

    09101021

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -47,7 +47,7 @@ function AppContent() {
           <Route path='/login' element={<Login />} />
           <Route path='/join' element={<Join />} />
 
-          {/* mypage 따로 나눌수 있음 */}
+          {/* mypage 따로 나눌수 있음 git test */}
           <Route path='/mypage/*' element={<Mypage />} />
           {/*<Route path='/mypage/mypageAccount' element={<MypageAccount />} />*/}
           {/*<Route path='/mypage/mypageUpdate' element={<MypageUpdate />} />*/}
