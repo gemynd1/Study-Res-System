d261ce94aa1165ac25e7a6848231fc506f03330e
ben <@jimin01>
2024-09-12 13:24:55 +0900

    feat: create teamDetail.js
    - teamdetail 밑작업

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,32 +1,33 @@
 // import logo from './logo.svg';
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
-import Header from './Components/Layout/Header';
-import Footer from './Components/Layout/Footer';
+import Header from "./Components/Layout/Header";
+import Footer from "./Components/Layout/Footer";
 
-import Main from './Components/Content/Page/Main';
-import Info from './Components/Content/Page/Info';
-import Board from './Components/Content/Page/Board';
-import Review from './Components/Content/Page/Review';
+import Main from "./Components/Content/Page/Main";
+import Info from "./Components/Content/Page/Info";
+import Board from "./Components/Content/Page/Board";
+import Review from "./Components/Content/Page/Review";
 import Mypage from "./Components/Content/Page/Mypage";
 import TeamDetail from "./Components/Content/Page/teamdetail";
 
-import MypageUpdate from './Components/Content/Mypage/MypageUpdate';
-import MypageBoard from './Components/Content/Mypage/MypageBoard';
-import MypageReview from './Components/Content/Mypage/MypageReview';
-import MypageAdd from './Components/Content/Mypage/MypageAdd';
-import Login from './Components/Content/Account/login';
-import Join from './Components/Content/Account/join';
+import MypageUpdate from "./Components/Content/Mypage/MypageUpdate";
+import MypageBoard from "./Components/Content/Mypage/MypageBoard";
+import MypageReview from "./Components/Content/Mypage/MypageReview";
+import MypageAdd from "./Components/Content/Mypage/MypageAdd";
+import Login from "./Components/Content/Account/login";
+import Join from "./Components/Content/Account/join";
 import MypageAccount from "./Components/Content/Mypage/MypageAccount";
 
 // import './App.css';
 
 function AppContent() {
-  const [hello, setHello] = useState('')
+  const [hello, setHello] = useState("");
   const location = useLocation();
-  const hideHeaderLogin = location.pathname === '/login' || location.pathname === '/join';
+  const hideHeaderLogin =
+    location.pathname === "/login" || location.pathname === "/join";
 
   // useEffect(() => {
   //   axios.get('/api/hello')
@@ -38,28 +39,28 @@ function AppContent() {
 
   return (
     <>
-        {!hideHeaderLogin && <Header />}
-        <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/info' element={<Info />} />
-          <Route path='/board' element={<Board />} />
-          <Route path='/review' element={<Review />} />
+      {!hideHeaderLogin && <Header />}
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/board" element={<Board />} />
+        <Route path="/review" element={<Review />} />
 
-          <Route path='/login' element={<Login />} />
-          <Route path='/join' element={<Join />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
 
-          {/* mypage 따로 나눌수 있음 git merge */}
-          <Route path='/mypage/*' element={<Mypage />} />
-          <Route path='/mypage/mypageAccount' element={<MypageAccount />} />
-          <Route path='/mypage/mypageUpdate' element={<MypageUpdate />} />
-          <Route path='/mypage/mypageBoard' element={<MypageBoard />} />
-          <Route path='/mypage/mypageReview' element={<MypageReview />} />
-          <Route path='/mypage/mypageAdd' element={<MypageAdd />} />
+        {/* mypage 따로 나눌수 있음 git merge */}
+        <Route path="/mypage/*" element={<Mypage />} />
+        <Route path="/mypage/mypageAccount" element={<MypageAccount />} />
+        <Route path="/mypage/mypageUpdate" element={<MypageUpdate />} />
+        <Route path="/mypage/mypageBoard" element={<MypageBoard />} />
+        <Route path="/mypage/mypageReview" element={<MypageReview />} />
+        <Route path="/mypage/mypageAdd" element={<MypageAdd />} />
 
-          {/* teamdetail페이지 */}
-          <Route path="/teamdetail" element={<TeamDetail/>} />
-        </Routes>
-        {!hideHeaderLogin && <Footer />}
+        {/* teamdetail페이지 */}
+        <Route path="/teamdetail" element={<TeamDetail />} />
+      </Routes>
+      {!hideHeaderLogin && <Footer />}
     </>
   );
 }
@@ -71,6 +72,5 @@ function App() {
     </BrowserRouter>
   );
 }
-
 
 export default App;
--- src/main/frontend/src/Components/Content/Page/teamdetail.js
+++ src/main/frontend/src/Components/Content/Page/teamdetail.js
@@ -1,11 +1,156 @@
 import React from "react";
-
+import teamDetail from "../../../style/teamDetail.css";
 const TeamDetail = () => {
-    return (
-        <>
-            teamdetail
-        </>
-    )
-}
+  return (
+    <div className="teamDetail">
+      <div className="teamDetail__main">
+        <div className="teamDetail__main-image"></div>
+        <div className="teamDetail__main-content">
+          <h1 className="teamDetail__main-content-title">h1제목</h1>
+          <h4 className="teamDetail__main-content-title-option">
+            h4내용|내용|내용|내용|내용
+          </h4>
+          <div className="teamDetail__main-header-line">라인</div>
+          <div className="teamDetail__main-content-text">
+            <h3 className="teamDetail__main-content-text-title">h3제목</h3>
+            <h4 className="teamDetail__main-content-text-text">h4내용</h4>
+          </div>
+        </div>
+        <div className="teamDetail__main-review">
+          <div className="teamDetail__main-review-profileIcon">
+            <img
+              className="profile"
+              src="/img/icon/profile.png"
+              alt="Profile"
+            />
+          </div>
+          <div className="teamDetail__main-review-wrap">
+            <div className="teamDetail__main-review-header">
+              <h2 className="teamDetail__main-review-name">h2이름</h2>
+              <img
+                className="teamDetail__main-review-star"
+                src="/img/icon/star.png"
+                alt="star"
+              ></img>
+            </div>
+            <h4 className="teamDetail__main-content-text-title">h4내용</h4>
+            <div className="teamDetail__main-review-photo">
+              <img
+                className="photos"
+                src="/img/icon/곧마감.png"
+                alt="star"
+              ></img>
+              <img
+                className="photos"
+                src="/img/icon/곧마감.png"
+                alt="star"
+              ></img>
+              <img
+                className="photos"
+                src="/img/icon/곧마감.png"
+                alt="star"
+              ></img>
+            </div>
+            <h2 className="teamDetail__main-review-name">h2이름</h2>
+            <h4 className="teamDetail__side-content-text-title">h4내용</h4>
+          </div>
+        </div>
+      </div>
+      <div className="teamDetail__side&buttons">
+        <div className="teamDetail__side">
+          <div className="teamDetail__side-header">
+            <h1 className="teamDetail__side-header-title">h1제목</h1>
+            <div className="teamDetail__side-header-line">라인-----------</div>
+          </div>
+          <div className="teamDetail__side-radio">
+            <div className="teamDetail__side-radio-button">O버튼</div>
+            <div className="teamDetail__side-radio-title"> 3~6인실</div>
+          </div>
+          <div className="teamDetail__side-image"></div>
+          <div className="teamDetail__side-description">
+            <h3 className="teamDetail__side-content-text-text">h3제목</h3>
+            <div className="teamDetail__side-header-line">
+              라인 -------------
+            </div>
+            <h3 className="teamDetail__side-content-text-text">h3제목</h3>
+            <div className="teamDetail__side-header-line">
+              라인 -------------
+            </div>
+            <h3 className="teamDetail__side-content-text-text">h3제목</h3>
+            <div className="teamDetail__side-header-line">
+              라인 -------------
+            </div>
+            <h3 className="teamDetail__side-content-text-text">h3제목</h3>
+            <div className="teamDetail__side-header-line">
+              라인 -------------
+            </div>
+          </div>
+          <div className="teamDetail__side-radio">
+            <div className="teamDetail__side-radio-button">O버튼</div>
+            <div className="teamDetail__side-radio-title">
+              {" "}
+              시간단위 예약하기
+            </div>
+          </div>
+          <div className="teamDetail__side-calendar">
+            <div className="teamDetail__side-calendar-header">달력</div>
+            <div className="teamDetail__side-calendar-content"></div>
+          </div>
+          <div className="teamDetail__side-legend">
+            <div className="teamDetail__side-legend-wrap">
+              <div className="teamDetail__side-legend-boxColor"></div>
+              <div className="teamDetail___side-legned-title">예약가능</div>
+            </div>
+            <div className="teamDetail__side-legend-wrap">
+              <div className="teamDetail__side-legend-boxColor"></div>
+              <div className="teamDetail___side-legned-title">예약불가</div>
+            </div>
+            <div className="teamDetail__side-legend-wrap">
+              <div className="teamDetail__side-legend-boxColor"></div>
+              <div className="teamDetail___side-legned-title">오늘</div>
+            </div>
+          </div>
+          <div className="teamDetail__side-header">
+            <h3 className="teamDetail__side-content-text-text">h3시간선택</h3>
+            <div className="teamDetail__side-header-line">
+              라인 -------------
+            </div>
+          </div>
+          <div className="teamDetail__side-choiceTime">시간선택버튼</div>
+          <div className="teamDetail__side-legend">
+            <div className="teamDetail__side-legend-wrap">
+              <div className="teamDetail__side-legend-boxColor"></div>
+              <div className="teamDetail___side-legned-title">예약가능</div>
+            </div>
+            <div className="teamDetail__side-legend-wrap">
+              <div className="teamDetail__side-legend-boxColor"></div>
+              <div className="teamDetail___side-legned-title">예약불가</div>
+            </div>
+            <div className="teamDetail__side-legend-wrap">
+              <div className="teamDetail__side-legend-boxColor"></div>
+              <div className="teamDetail___side-legned-title">오늘</div>
+            </div>
+          </div>
 
+          <div className="teamDetail__side-header">
+            <h3 className="teamDetail__side-content-text-text">h3총예약인원</h3>
+            <div className="teamDetail__side-header-line">
+              라인 -------------
+            </div>
+          </div>
+          <div className="teamDetail__side-count-control">인원증감버튼</div>
+        </div>
+        <div className="teamDetail__buttons">
+          <div className="teamDitail__call-chating">
+            <div className="teamDetail__side-callButton">전화</div>
+            <div className="teamDetail__side-chatingButton">채팅</div>
+          </div>
+          <div className="teamDetail__side-reserveButton">예약가능</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+<style>.teamDetail{}</style>;
 export default TeamDetail;
