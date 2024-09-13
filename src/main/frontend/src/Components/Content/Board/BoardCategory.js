933108959a174a03e2c9b0cab722d735604caa6b
BAEK JI MIN <baekjimin0301@gmail.com>
2024-09-14 02:13:28 +0900

    feat: post페이지 기본완성 & fix: board페이지 수정중

--- src/main/frontend/src/Components/Content/Board/BoardCategory.js
+++ src/main/frontend/src/Components/Content/Board/BoardCategory.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../../style/boardCategory.css";
 
+const contentClick = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+}
+
 const BoardCategory = () => {
     return (
         <>
@@ -36,7 +40,7 @@ const BoardCategory = () => {
                             <img src="/img/icon/곧마감.png" alt="곧마감" className="board-category-1-headerbar-img"></img>
                             <p className="board-category-1-headerbar-title">시작이 얼마남지 않은 모임을 확인해보세요! </p>
                         </div>
-                        <Link to="/board/post/1" className="board-category-1-content">
+                        <div className="board-category-1-content" onClick={() => contentClick("/board/post/1")}>
                             <p className="board-category-1-content-title">이 글의 제목입니다.</p>
                             <p className="board-category-1-content-detail">이 글의 내용부분입니다 이 글의 내용부분입니다 이 글의 내용부분입니다.</p>
                             <div className="board-category-1-content-info">
@@ -50,7 +54,7 @@ const BoardCategory = () => {
                                 <img src="/img/icon/group.png" alt="그룹이미지" className="board-category-1-content-info2-group" />
                                 <p className="board-category-1-content-info2-count">1 / 5 명 (최소 2명)</p>
                             </div>
-                        </Link>
+                        </div>
                         <div className="moreButton">
                             <div className="moreButton-section">
                                 <span className="moreButton-text">MORE</span>
--- src/main/frontend/src/Components/Content/Board/Post.js
+++ src/main/frontend/src/Components/Content/Board/Post.js
@@ -1,87 +1,131 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import '../../../style/post.css';
 
-import IconButton from '@mui/material/IconButton';
+// 댓글 더보기 버튼 import
+import '../../../style/post.css';
+import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Fade from '@mui/material/Fade';
+
+// 페이지네이션 import
 import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
 
-const options = [
-    'None',
-    'Atria',
-    'Callisto',
-    'Dione',
-    'Ganymede',
-    'Hangouts Call',
-    'Luna',
-    'Oberon',
-    'Phobos',
-    'Pyxis',
-    'Sedna',
-    'Titania',
-    'Triton',
-    'Umbriel',
-];
+// 모달import
+import Box from '@mui/material/Box';
+// import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
 
-const ITEM_HEIGHT = 48;
 
-function LongMenu() {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-  
-    return (
-      <div className="longmenu-style">
-        <IconButton
-          aria-label="more"
-          id="long-button"
-          aria-controls={open ? 'long-menu' : undefined}
-          aria-expanded={open ? 'true' : undefined}
-          aria-haspopup="true"
-          onClick={handleClick}
-        >
-          <MoreVertIcon />
-        </IconButton>
-        <Menu
-          id="long-menu"
-          MenuListProps={{
-            'aria-labelledby': 'long-button',
-          }}
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          slotProps={{
-            paper: {
-              style: {
-                maxHeight: ITEM_HEIGHT * 4.5,
-                width: '20ch',
-              },
-            },
-          }}
-        >
-          {options.map((option) => (
-            <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
-              {option}
-            </MenuItem>
-          ))}
-        </Menu>
-      </div>
-    );
+
+// 댓글 더보기 버튼
+function FadeMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className="fademenu">
+      <Button
+        id="fade-button"
+        aria-controls={open ? 'fade-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        <img src="/img/icon/seemore.png" alt="seemoreicon" className="seemore-icon" />
+      </Button>
+      <Menu
+        id="fade-menu"
+        MenuListProps={{
+          'aria-labelledby': 'fade-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Fade}
+      >
+        <MenuItem onClick={handleClose}>신고</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
 }
 
+// 댓글 페이지네이션
 function BasicPagination() {
     return (
     <Pagination count={10} />
     );
-  }
+}   
+
+//모달
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 562,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    borderRadius: 6,
+    boxShadow: 24,
+};
+
+function BasicModal() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+  
+    return (
+      <div className="QandA-button" >
+        <img src="/img/icon/write.png" alt="QandAicon" className="QandA-button-icon" />
+        <div className="QandA-button-text" onClick={handleOpen}>질문 작성하기</div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+
+            <div className="modal-header">
+                <span className="modal-header-title">질문 작성하기</span>
+            </div>
+
+            <div className="modal-title-section">
+                <span className="modal-title">질문</span>
+                <span className="modal-title-count1">0자</span>
+                <span className="modal-title-count2">/200자</span>
+            </div>
+
+            <input />
+
+            <div className="modal-caution-section">
+                <img src="/img/icon/!(modal).png" alt="!" className="modal-caution-icon" />
+                <span className="modal-caution-text">단, 공간 및 예약에 대한 문의가 아닌 글은 무통보 삭제될 수 있습니다.</span>
+            </div>
+
+            <div className="modal-button-section">
+                <div onClick={handleClose} className="modal-cancel-button">
+                    <span className="modal-cancel-text">취소</span>
+                </div>
+                <div onClick={handleClose} className="modal-active-button">
+                    <span className="modal-active-text">등록</span>
+                </div>
+            </div>
+
+          </Box>
+        </Modal>
+      </div>
+    );
+}
 
 const Post = () => {
     return (
@@ -179,10 +223,8 @@ const Post = () => {
                     <div className="post-comment-header">
                         <span className="QandA">Q&A</span>
                         <span className="QandA-count">(2개)</span>
-                        <div className="QandA-button">
-                            <img src="/img/icon/write.png" alt="QandAicon" className="QandA-button-icon" />
-                            <span className="QandA-button-text">질문 작성하기</span>
-                        </div>
+                        <BasicModal />
+                        {/* <span className="QandA-button-text">질문 작성하기</span> */}
                     </div>
                     <div className="post-comment-main">
                         <div className="post-question">
@@ -197,7 +239,7 @@ const Post = () => {
                                 <span className="comment-loaddate">2024-09-05 21:49:17</span>
                             </div>
 
-                            <LongMenu />
+                            <FadeMenu />
 
                             {/* <img src="/img/icon/seemore.png" alt="seemoreicon" className="comment-seemore" />
                             <div className="comment-seemore-detail">
