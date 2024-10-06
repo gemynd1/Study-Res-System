4f3f455609d89aebc6f51f66ce8f61787345055f
muse7244 <taerangkim0116@gmail.com>
2024-10-07 00:13:33 +0900

    feat : auth 로그인 회원가입 로그아웃 controller 작업 완료

--- src/main/java/com/project/SnakeDev/controller/AuthController.java
+++ src/main/java/com/project/SnakeDev/controller/AuthController.java
@@ -4,11 +4,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.SnakeDev.config.VOMapper;
 import com.project.SnakeDev.service.AuthService;
 import com.project.SnakeDev.vo.AuthVo;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Member;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -65,14 +69,35 @@ public class AuthController {
     @GetMapping("/login")
     public ResponseEntity<Object> login(
             @RequestParam("id") String MemberId,
-            @RequestParam("pw") String MemberPw) {
-        System.out.println(MemberId);
-        System.out.println(MemberPw);
+            @RequestParam("pw") String MemberPw,
+            HttpServletRequest request,
+            HttpSession session) {
+
+        session = request.getSession();
         String MemberName = authService.login(MemberId, MemberPw);
+        Map<String, String> sessionInfo = new HashMap<>();
         if(MemberName != null) {
-            return ResponseEntity.ok(MemberName);
+            session.setAttribute("id", MemberId);
+            session.setAttribute("name", MemberName);
+
+            sessionInfo.put("id", MemberId);
+            sessionInfo.put("name", MemberName);
+
+            return ResponseEntity.ok(sessionInfo);
         } else {
-            return ResponseEntity.ok("로그인에 실패하였습니다.");
+            return ResponseEntity.ok(false);
         }
     }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Object> logout(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        session.invalidate();
+
+        return ResponseEntity.ok(true);
+    }
+
+
+    // https://velog.io/@fever-max/Spring-Boot-React-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-Security-%EA%B8%B0%EB%B3%B8-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EB%B0%8F-%EA%B6%8C%ED%95%9C-%EA%B5%AC%ED%98%84
+
 }
